1669135466e6346112fdf031311ecbb5cc43d20b
otamm <oivatom@gmail.com>
2015-09-12 01:31:55 -0300

    Added dynamic storage and retrieval of notes

--- MakeSchoolNotes/ViewControllers/NotesViewController.swift
+++ MakeSchoolNotes/ViewControllers/NotesViewController.swift
@@ -13,10 +13,32 @@ class NotesViewController: UIViewController {
     
     @IBOutlet weak var tableView:UITableView!;
     
+    var notes: Results<Note>! {
+        didSet {
+            // Whenever notes update, update the table view
+            self.tableView?.reloadData();
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.tableView.dataSource = self;
+        
+        // dynamically adds an arbitrary note to test table view reloading and displaying new info gotten after some note addition:
+        let myNote = Note();
+        myNote.title = "Some useless testing text";
+        myNote.content = "raw static information is worth nothing.";
+        
+        // establishes a connection to Realm.
+        let realm = Realm();
+        // write method will store information that can be afterwards retrieved; this storage logic will be provided via a closure.
+        realm.write() {
+            realm.add(myNote);
+        }
+        
+        // fetches saved notes and assigns the fetch value to the 'notes' variable:
+        self.notes = realm.objects(Note);
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,14 +58,24 @@ extension NotesViewController:UITableViewDataSource {
         // the 'NoteCell' is an unique identifier for the NoteTableViewCell, allowing the use of many custom designs, each associated with its identifier. Every line printing 'Hello World' will be a 'NoteCell' NoteTableViewCell.
         
         let row = indexPath.row;
-        cell.setTitle("Hello World");
-        cell.setDate("10th", month: "October", year: "5776");
+        //cell.setTitle("Hello World");
+        //cell.setDate("10th", month: "October", year: "5776");
+        let note = self.notes[row] as Note; // fetches the specific note for that row and loads it as a Note instance.
+        cell.note = note; // assigns this note instance to the 'note' property of cell and returns this cell with the 'note' property assigned.
         
         return cell;
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5// number of rows to be returned. '5' is only an arbitrary test.
+        //return 5// number of rows to be returned. '5' is only an arbitrary test.
+        // returns the number of notes currently stored (if the total size of 'notes' is bigger than 0, returns the number of items stored in the 'notes' property. Otherwise, return 0):
+        return Int(self.notes?.count ?? 0);
+        /* equivalent to
+        if let notes = notes {
+            return Int(notes.count);
+        } else {
+            return 0;
+        }*/
     }
 }
 
--- MakeSchoolNotes/Views/NoteTableViewCell.swift
+++ MakeSchoolNotes/Views/NoteTableViewCell.swift
@@ -11,6 +11,29 @@ import UIKit
 
 class NoteTableViewCell: UITableViewCell {
     
+    
+    // initialize the date formatter only once, using a static computed property;
+    // since this code will be the same used for all date table rows, it is worthy to initialize it only once.
+    static var dateFormatter: NSDateFormatter = {
+        var formatter = NSDateFormatter();
+        formatter.dateFormat = "yyyy-MM-dd";
+        return formatter;
+    } ();
+    
+    var note: Note? {
+        // called whenever this note variable is modified. For example, if the note gets edited, this will be called and update the title or date label in the corresponding note cell.
+        didSet {
+            // sets this cell's titleLabel, dateLabel and note to these constants; if the note, the titleLabel aaand dateLabel variables are set, execute the code block below.
+            if let note = self.note, titleLabel = self.titleLabel, dateLabel = self.dateLabel {
+                // sets the note's data cell title label to the same value as the title associated with the note instance.
+                //self.titleLabel.text = note.title; 
+                self.setTitle(self.note!.title);
+                // same, with the date label.
+                self.dateLabel.text = NoteTableViewCell.dateFormatter.stringFromDate(note.modificationDate);
+            }
+        }
+    }
+    
     // title of the cell.
     @IBOutlet weak var titleLabel:UILabel!;
     // last modification of the cell.
