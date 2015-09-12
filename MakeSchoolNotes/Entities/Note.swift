3dc806889685de41fdc7aa6b1009962b0093b4ae
otamm <oivatom@gmail.com>
2015-09-12 00:58:21 -0300

    Added data columns to Note entity

--- MakeSchoolNotes/Entities/Note.swift
+++ MakeSchoolNotes/Entities/Note.swift
@@ -6,10 +6,15 @@
 //  Copyright (c) 2015 MakeSchool. All rights reserved.
 //
 
-import Foundation
-import RealmSwift
+import Foundation;
+import RealmSwift;
 
 class Note : Object {
-
+    
+    dynamic var title:String = ""; // dynamic here indicates to the compiler that this property will be set at runtime.
+    
+    dynamic var content:String = "";
+    
+    dynamic var modificationDate = NSDate();
     
 }
