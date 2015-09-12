131e4c9b9a2c2ea41b17980cf1b4c606fcbd8dba
otamm <oivatom@gmail.com>
2015-09-11 22:44:54 -0300

    Added extension to NotesViewController

--- MakeSchoolNotes/ViewControllers/NotesViewController.swift
+++ MakeSchoolNotes/ViewControllers/NotesViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 import RealmSwift
 
 class NotesViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView:UITableView!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self;
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +24,23 @@ class NotesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+// extends the above class with functionality from another class.
+extension NotesViewController:UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // dequeueReusableCellWithIdentifier is a method of UITableView class that tries to find a reusable cell to improve initialization performance. When this cell scrolls off the screen, it will be added to an internal collection of cells that can be recycled; if none exists (as it will be the case when the app is first run), the method will create new instances of UITableViewCells, in this case, a custom subclass of UITableViewCell (NoteTableViewCell, located in the 'Views' directory) will be used.
+        let cell = tableView.dequeueReusableCellWithIdentifier("NoteCell", forIndexPath: indexPath) as! NoteTableViewCell //1 // the '1' is an unique identifier for the NoteTableViewCell, allowing the use of many custom designs, each associated with its identifier.
+        
+        let row = indexPath.row;
+        cell.textLabel?.text = "Hello World";
+        
+        return cell;
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
 }
 
